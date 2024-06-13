e6797e0de51f8ce48a5d0a9ba6c22562e9704243
piatoss <crewe1746@naver.com>
2024-06-13 14:29:12 +0900

    Add loading spinner on loading home page

--- frontend/components/home/index.tsx
+++ frontend/components/home/index.tsx
@@ -4,10 +4,15 @@ import React from "react";
 import Dashboard from "./Dashboard";
 import { usePrivy } from "@privy-io/react-auth";
 import { useRouter } from "next/navigation";
+import { Spinner } from "@chakra-ui/react";
 
 const Home = () => {
-  const { authenticated } = usePrivy();
+  const { ready, authenticated } = usePrivy();
   const router = useRouter();
+
+  if (!ready) {
+    return <Spinner color="brand.darkChocolate" size="lg" />;
+  }
 
   if (!authenticated) {
     router.push("/login");
