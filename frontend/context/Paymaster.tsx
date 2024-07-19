f55ac4d42f91ed2e6939ef10a0246a27035c2a11
piatoss <crewe1746@naver.com>
2024-07-19 19:19:48 +0900

    feat: add approval based paymaster feature

--- frontend/context/Paymaster.tsx
+++ frontend/context/Paymaster.tsx
@@ -1,16 +1,22 @@
 import PaymasterModal from "@/components/paymaster";
 import useZkSyncClient from "@/hooks/useZkSyncClient";
+import IERC20_ABI from "@/libs/IERC20";
+import IERC721_ABI from "@/libs/IERC721";
+import { LIBRO_NFT_ADDRESS } from "@/libs/LibroNFT";
 import {
+  LIBRO_ERC20_PAYMASTER_ABI,
+  LIBRO_ERC20_PAYMASTER_ADDRESS,
   LIBRO_PAYMASTER_ABI,
   LIBRO_PAYMASTER_ADDRESS,
 } from "@/libs/LibroPaymaster";
 import { formatBigNumber, formatEstimateFee } from "@/libs/utils";
-import { PaymasterRequest, PaymasterType } from "@/types";
+import { ERC20TokenMetadata, PaymasterRequest, PaymasterType } from "@/types";
 import { useDisclosure, useToast } from "@chakra-ui/react";
 import { useQueries, useQuery } from "@tanstack/react-query";
 import { createContext, useState } from "react";
 import {
   EstimateFeeReturnType,
+  getApprovalBasedPaymasterInput,
   getGeneralPaymasterInput,
   zkSyncSepoliaTestnet,
 } from "viem/zksync";
@@ -38,48 +44,25 @@ const PaymasterProvider = ({ children }: { children: React.ReactNode }) => {
 
   // State for the paymaster (general, approval)
   const [paymasterType, setPaymasterType] = useState<PaymasterType>("general");
+  const [selectedToken, setSelectedToken] = useState<ERC20TokenMetadata | null>(
+    null
+  );
+  const SupportedTokensList: ERC20TokenMetadata[] = [
+    {
+      address: "0xAe045DE5638162fa134807Cb558E15A3F5A7F853",
+      decimals: 6,
+      symbol: "USDC",
+      name: "USDC",
+    },
+  ];
+  const isGeneralPaymaster = paymasterType === "general";
 
   // State for the loading status and transaction result
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [txStatus, setTxStatus] = useState<"success" | "reverted" | "">("");
   const [txHash, setTxHash] = useState<string>("");
 
-  // Function to get the estimate fee of zkSync network for a transaction
-  const getEstimateFee = async (): Promise<EstimateFeeReturnType> => {
-    if (!publicClient || !wallet) {
-      throw new Error("Request not found");
-    }
-
-    if (!request) {
-      throw new Error("Request not found");
-    }
-
-    // TODO: Get the paymaster input based on the paymaster type
-
-    const paymasterInput = getGeneralPaymasterInput({
-      innerInput: "0x",
-    });
-
-    return await publicClient.estimateFee({
-      account: request.from || (wallet.address as `0x${string}`),
-      to: request.to,
-      data: request.data,
-      value: request.value,
-      paymaster: LIBRO_PAYMASTER_ADDRESS,
-      paymasterInput,
-    });
-  };
-
-  const { data: estimateFee } = useQuery({
-    queryKey: ["estimateFee", wallet?.address],
-    queryFn: getEstimateFee,
-    enabled: !!publicClient && !!wallet && !!request, // Only fetch when the wallet and request are available
-    refetchInterval: 3000, // Refetch every 3 seconds
-  });
-
-  const { gasPrice, fee, cost } = formatEstimateFee(estimateFee);
-
-  // Function to open the paymaster modal
+  // ==================== PAYMASTER MODAL FUNCTIONS ====================
   const openPaymasterModal = (
     request: PaymasterRequest,
     callback?: () => void
@@ -89,72 +72,26 @@ const PaymasterProvider = ({ children }: { children: React.ReactNode }) => {
     onOpen();
   };
 
-  // Function to close the paymaster modal
   const closePaymasterModal = () => {
+    // Cache the callback and status
     const fn = callback;
     const status = txStatus;
 
+    // Reset the state
     setRequest(null);
+    setPaymasterType("general");
     setCallback(() => {});
     setTxStatus("");
     setTxHash("");
     onClose();
 
+    // Call the callback function if the transaction was successful
     if (status === "success") {
-      fn(); // Call the callback function
+      fn();
     }
   };
 
-  const confirmPayment = async () => {
-    try {
-      if (!wallet || !zkSyncClient) {
-        throw new Error("Wallet or zkSync client not found");
-      }
-
-      if (!request) {
-        throw new Error("Request not found");
-      }
-
-      setIsLoading(true);
-
-      // TODO: Get the paymaster input based on the paymaster type
-
-      // Get the paymaster input
-      const paymasterInput = getGeneralPaymasterInput({
-        innerInput: "0x",
-      });
-
-      // Send the transaction
-      const hash = await zkSyncClient.sendTransaction({
-        account: request.from || (wallet.address as `0x${string}`),
-        to: request.to,
-        data: request.data,
-        value: request.value,
-        chain: zkSyncSepoliaTestnet,
-        paymaster: LIBRO_PAYMASTER_ADDRESS,
-        paymasterInput,
-      });
-
-      // Wait for the transaction receipt
-      const receipt = await publicClient.waitForTransactionReceipt({ hash });
-
-      // Update the result
-      setTxHash(hash);
-      setTxStatus(receipt.status);
-    } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : "Unknown error";
-      toast({
-        title: "Error",
-        description: errorMessage,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  // ==================== GENERAL PAYMASTER QUERIES ====================
 
   // Function to get the daily limit of the paymaster
   const getDailyLimit = async (): Promise<bigint> => {
@@ -206,14 +143,169 @@ const PaymasterProvider = ({ children }: { children: React.ReactNode }) => {
       throw new Error("Public client not found");
     }
 
+    const balance = await publicClient.readContract({
+      address: LIBRO_NFT_ADDRESS,
+      abi: IERC721_ABI,
+      functionName: "balanceOf",
+      args: [account],
+    });
+
+    return balance > BigInt(0);
+  };
+
+  // ==================== APPROVAL BASED PAYMASTER FUNCTIONS ====================
+
+  const getEthPriceInToken = async (
+    tokenAddress: `0x${string}`,
+    ethAmount: bigint
+  ): Promise<readonly [bigint, number]> => {
+    if (!publicClient) {
+      throw new Error("Public client not found");
+    }
+
     return await publicClient.readContract({
-      address: LIBRO_PAYMASTER_ADDRESS,
-      abi: LIBRO_PAYMASTER_ABI,
-      functionName: "isNftOwner",
+      address: LIBRO_ERC20_PAYMASTER_ADDRESS,
+      abi: LIBRO_ERC20_PAYMASTER_ABI,
+      functionName: "getEthPriceInToken",
+      args: [tokenAddress, ethAmount],
+    });
+  };
+
+  // ==================== TOKEN QUERY ====================
+  const getTokenBalance = async (
+    tokenAddress: `0x${string}`,
+    account: `0x${string}`
+  ): Promise<bigint> => {
+    if (!publicClient) {
+      throw new Error("Public client not found");
+    }
+
+    return await publicClient.readContract({
+      address: tokenAddress,
+      abi: IERC20_ABI,
+      functionName: "balanceOf",
       args: [account],
     });
   };
 
+  // ==================== ESTIMATE FEE QUERY ====================
+  // Function to get the estimate fee of zkSync network for a transaction
+  const getEstimateFee = async (): Promise<EstimateFeeReturnType> => {
+    if (!publicClient || !wallet) {
+      throw new Error("Required client or wallet not found");
+    }
+
+    if (!request) {
+      throw new Error("Request not found");
+    }
+
+    let paymaster = LIBRO_PAYMASTER_ADDRESS;
+    let paymasterInput = getGeneralPaymasterInput({
+      innerInput: "0x",
+    });
+
+    // Get the paymaster input based on the paymaster type
+    if (!isGeneralPaymaster) {
+      if (!selectedToken) {
+        throw new Error("Token not found");
+      }
+
+      const tokenBalance = await getTokenBalance(
+        selectedToken.address,
+        wallet.address as `0x${string}`
+      );
+
+      paymaster = LIBRO_ERC20_PAYMASTER_ADDRESS;
+      paymasterInput = getApprovalBasedPaymasterInput({
+        innerInput: "0x",
+        minAllowance: tokenBalance,
+        token: selectedToken.address,
+      });
+    }
+
+    return await publicClient.estimateFee({
+      account: request.from || (wallet.address as `0x${string}`),
+      to: request.to,
+      data: request.data,
+      value: request.value,
+      paymaster,
+      paymasterInput,
+    });
+  };
+
+  // ==================== PAYMENT CONFIRMATION FUNCTION ====================
+  const confirmPayment = async () => {
+    try {
+      if (!wallet || !zkSyncClient) {
+        throw new Error("Wallet or zkSync client not found");
+      }
+
+      if (!request) {
+        throw new Error("Request not found");
+      }
+
+      setIsLoading(true);
+
+      // Get the paymaster input
+      let paymaster = LIBRO_PAYMASTER_ADDRESS;
+      let paymasterInput = getGeneralPaymasterInput({
+        innerInput: "0x",
+      });
+
+      // Get the paymaster input based on the paymaster type
+      if (!isGeneralPaymaster) {
+        if (!selectedToken) {
+          throw new Error("Token not found");
+        }
+
+        const estimateFee = await getEstimateFee();
+        const cost = estimateFee.gasLimit * estimateFee.maxFeePerGas;
+        const tokenAmount = await getEthPriceInToken(
+          selectedToken.address,
+          cost
+        );
+
+        paymaster = LIBRO_ERC20_PAYMASTER_ADDRESS;
+        paymasterInput = getApprovalBasedPaymasterInput({
+          innerInput: "0x",
+          minAllowance: tokenAmount[0],
+          token: selectedToken.address,
+        });
+      }
+
+      // Send the transaction
+      const hash = await zkSyncClient.sendTransaction({
+        account: request.from || (wallet.address as `0x${string}`),
+        to: request.to,
+        data: request.data,
+        value: request.value,
+        chain: zkSyncSepoliaTestnet,
+        paymaster,
+        paymasterInput,
+      });
+
+      // Wait for the transaction receipt
+      const receipt = await publicClient.waitForTransactionReceipt({ hash });
+
+      // Update the result
+      setTxHash(hash);
+      setTxStatus(receipt.status);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "Unknown error";
+      toast({
+        title: "Error",
+        description: errorMessage,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // ==================== GENERAL PAYMASTER QUERIES ====================
   const [
     dailyLimitQuery,
     checkDailyLimitQuery,
@@ -224,7 +316,7 @@ const PaymasterProvider = ({ children }: { children: React.ReactNode }) => {
       {
         queryKey: ["dailyLimit"],
         queryFn: getDailyLimit,
-        enabled: !!publicClient,
+        enabled: !!publicClient && isGeneralPaymaster,
         refetchInterval: 3000,
       },
       {
@@ -232,7 +324,7 @@ const PaymasterProvider = ({ children }: { children: React.ReactNode }) => {
         queryFn: async () => {
           return await checkDailyLimit(wallet!.address as `0x${string}`);
         },
-        enabled: !!publicClient && !!wallet,
+        enabled: !!publicClient && !!wallet && isGeneralPaymaster,
         refetchInterval: 3000,
       },
       {
@@ -240,7 +332,7 @@ const PaymasterProvider = ({ children }: { children: React.ReactNode }) => {
         queryFn: async () => {
           return await getIsBanned(wallet!.address as `0x${string}`);
         },
-        enabled: !!publicClient && !!wallet,
+        enabled: !!publicClient && !!wallet && isGeneralPaymaster,
         refetchInterval: 3000,
       },
       {
@@ -254,11 +346,34 @@ const PaymasterProvider = ({ children }: { children: React.ReactNode }) => {
     ],
   });
 
+  // ==================== APPROVAL BASED PAYMASTER QUERY ====================
+  const ethPriceInTokenQuery = useQuery({
+    queryKey: ["ethPriceInToken", selectedToken?.address],
+    queryFn: async () => {
+      const estimateFee = await getEstimateFee();
+
+      return await getEthPriceInToken(selectedToken!.address, BigInt(1));
+    },
+    enabled: !!publicClient && !!selectedToken && !isGeneralPaymaster,
+    refetchInterval: 3000,
+  });
+
+  // ==================== ESTIMATE FEE QUERY ====================
+  const estimateFeeQuery = useQuery({
+    queryKey: ["estimateFee", wallet?.address],
+    queryFn: getEstimateFee,
+    enabled: !!publicClient && !!wallet && !!request,
+    refetchInterval: 3000,
+  });
+
+  // ==================== RENDER GENERAL PAYMASTER DATA ====================
   const dailyLimit = formatBigNumber(dailyLimitQuery.data);
   const [canResetDailyTxCount, hasReachedDailyLimit, dailyTxCount] =
     checkDailyLimitQuery.data || [false, false, BigInt(0)];
   const isBanned = isBannedQuery.data || false;
   const isNftOwner = isNftOwnerQuery.data || false;
+
+  const { gasPrice, fee, cost } = formatEstimateFee(estimateFeeQuery.data);
 
   return (
     <PaymasterContext.Provider
--- frontend/types/index.d.ts
+++ frontend/types/index.d.ts
@@ -2,6 +2,13 @@ import { Account, Address } from "viem";
 
 type PaymasterType = "general" | "approval";
 
+type ERC20TokenMetadata = {
+  address: `0x${string}`;
+  decimals: number;
+  symbol: string;
+  name: string;
+};
+
 interface PaymasterRequest {
   name: string; // The name of the request
   from?: Account | Address; // The account or address from which the transaction is sent
@@ -10,4 +17,4 @@ interface PaymasterRequest {
   value?: bigint; // The value of the transaction
 }
 
-export { PaymasterType, PaymasterRequest };
+export { PaymasterType, PaymasterRequest, ERC20TokenMetadata };
